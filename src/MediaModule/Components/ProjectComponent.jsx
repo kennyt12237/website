16d0a7b96648c560bd9fd07a9ee7097073db690d
unknown <kennyt12237@gmail.com>
2022-03-11 00:20:49 +1300

    Revert "Tried both ways but with problems (chained effects)"
    This reverts commit a97bf84a127b62c79bffc47df9703b1626e0dde9.

--- src/MediaModule/Components/ProjectComponent.jsx
+++ src/MediaModule/Components/ProjectComponent.jsx
@@ -22,17 +22,8 @@ export default function ProjectComponent(props) {
 				}
 			}
 		}
-	},[media])
+	},[media, setSelectedText, srcs])
 
-
-	useEffect(() => {
-		setMedia([
-			srcs[selectedText],
-			...srcs.slice(0, selectedText),
-			...srcs.slice(selectedText + 1, selectedText.length)
-		])
-	}, [selectedText])
-	
 	return (
 		<div className='project-container'>
 			<ProjectText title={title} list={list} state={[selectedText, setSelectedText]}/>
