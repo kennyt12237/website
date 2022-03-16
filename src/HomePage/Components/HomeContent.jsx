54793ffd7a7708ca6c4c39fb4f6590a65db7f4ff
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-03-16 15:47:10 +1300

    Implemented the project content screen; project content component

---
+++ src/HomePage/Components/HomeContent.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import '../scss/HomeContent.scss';
+
+export default function HomeContent(props) {
+
+	return (
+		<div className='info-container'>
+				<div className='info-container__image'>
+
+				</div>
+				<div className='info-container__content'>
+					<div className='info-container__content__greeting'>
+						Hello!
+					</div>
+					<div className='info-container__content__statement'>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p> 
+
+						<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+					<div className='info-container__content__contact'>
+						<div className='info-container__content__contact__wallet'>
+							<img className="info-container__content__contact__wallet__image" src="metamask.png" alt="Metamask Logo" />
+							<div className="info-container__content__contact__wallet__address">
+								<p> Wallet address: </p>
+								<p> ***REMOVED*** </p>
+							</div>
+						</div>
+						<div className='info-container__content__contact__social'>
+							<img src={"./linkedin.svg"} alt="linkedIn" />
+							<img src={"./twitter.svg"} alt="twitter" />
+							<img src={"./telegram.svg"} alt="telegram" />
+							<img src={"./medium.svg"} alt="medium" />
+						</div>
+					</div>
+				</div>
+			</div>
+	)
+
+}
---
+++ src/Media/Components/ProjectText.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../scss/ProjectText.scss';
+
+export default function ProjectText(props) {
+
+	const { title, texts } = props;
+
+	return (
+		<div className="project-text-container">
+			<p className="project-text-container__title"> {title} </p>
+			<p className="project-text-container__text"> { texts } </p>
+
+		</div>
+	)
+}
