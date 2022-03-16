b1a2c38e452e0e966491ac0702d8e8c6a3f4bbe8
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-03-16 15:47:10 +1300

    Implemented the project content screen; project content component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.scss';
-import ProjectDisplayer from './MediaModule/ProjectDisplayer';
-import { projects } from './MediaModule/DummyProject/dummyProjectsOne';
+import HomeContent from './HomePage/Components/HomeContent';
+import ProjectContent from './Media/Components/ProjectContent';
+import { data } from './Media/DummyData/dummyTextOne';
 
 function App() {
   return (
@@ -16,40 +17,8 @@ function App() {
 				<img className="wallet-container__image" src="metamask.png" alt="Metamask Logo" />
 				<p>Connect Wallet </p>
 			</div>
-			<div className='info-container'>
-				<div className='info-container__image'>
-
-				</div>
-				<div className='info-container__content'>
-					<div className='info-container__content__greeting'>
-						Hello!
-					</div>
-					<div className='info-container__content__statement'>
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p> 
-
-						<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					</div>
-					<div className='info-container__content__contact'>
-						<div className='info-container__content__contact__wallet'>
-							<img className="info-container__content__contact__wallet__image" src="metamask.png" alt="Metamask Logo" />
-							<div className="info-container__content__contact__wallet__address">
-								<p> Wallet address: </p>
-								<p> E9873D79C6D87DC0FB6A5778633389F445 </p>
-							</div>
-						</div>
-						<div className='info-container__content__contact__social'>
-							<img src={"./linkedin.svg"} alt="linkedIn" />
-							<img src={"./twitter.svg"} alt="twitter" />
-							<img src={"./telegram.svg"} alt="telegram" />
-							<img src={"./medium.svg"} alt="medium" />
-						</div>
-					</div>
-					
-				</div>
-			</div>
-			{/* <ProjectDisplayer  projects={projects} /> */}
 		</div>
-
+		<ProjectContent projectContent={data.projectContent} web3Message={data.web3Message} mediaSrc={data.mediaSrc}/>
 	</div>
   );
 }
