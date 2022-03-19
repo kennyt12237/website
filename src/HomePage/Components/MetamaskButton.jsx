e35a240848c860ef25c9a0830f304a6848263015
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-03-19 16:14:49 +1300

    Added connect/disconnect functionality when button is clicked. Additional metamask methods added

--- src/HomePage/Components/MetamaskButton.jsx
+++ src/HomePage/Components/MetamaskButton.jsx
@@ -1,34 +1,58 @@
 import React, { useContext } from 'react';
 import { MetamaskContext } from '../../Web3/context/MetamaskProvider';
+import { WalletContext } from '../../Web3/context/WalletCoxtentProvider';
 
 export default function MetamaskButton(props) {
 
-	const { connectToMetamaskAndSetBasicFunc } = useContext(MetamaskContext);
+	const { connectToMetamaskAndSetBasicFunc, disconnectFromMetamask } = useContext(MetamaskContext);
+	const { getWalletAddress, getConnectedStatus } = useContext(WalletContext);
 
-	const providerNotDetected = () => {
+	const providerNotDetected = (result) => {
+		console.log(result);
 		alert("No Provider detected")
 	};
-	const providerNotEthereum = () => {
+	const providerNotEthereum = (result) => {
+		console.log(result);
 		alert("Provider is not Ethereum");
 	};
 
-	const handleAccountChanged = () => {
-		alert("Account changed");
-		window.location.reload();
+	const handleAccountChanged = (account) => {
+		console.log("Account connected or changed");
 	};
 
-	const handleAccountFailed = () => {
+	const handleAccountFailed = (error) => {
+		console.log(error);
 		alert("Account failed to connect");
 	};
 
-	const handleChainChanged = () => {
-		alert("Switched to chain");
+	const handleChainChanged = (chain) => {
+		console.log(chain);
+		alert("Switched chain to " + chain);
 	};
 
+	const handleMetamaskButtonClicked = () => {
+		connectToMetamaskAndSetBasicFunc(providerNotDetected, providerNotEthereum, handleAccountChanged, handleAccountFailed, handleChainChanged);
+	}
+
+	const handleDisconnectButtonClicked = () => {
+		disconnectFromMetamask();
+		console.log("Disconnected");
+	}
+
+
 	return (
-		<div className='wallet-container' onClick={() => connectToMetamaskAndSetBasicFunc(providerNotDetected, providerNotEthereum, handleAccountChanged, handleAccountFailed, handleChainChanged)} >
-			<img className="wallet-container__image" src="metamask.png" alt="Metamask Logo" />
-			<p> Connect Wallet </p>
+		<div>
+			{ getConnectedStatus() ? (
+			<div className='wallet-container' onClick={() => handleDisconnectButtonClicked()} >
+				<img className="wallet-container__image" src="metamask.png" alt="Metamask Logo" />
+ 				<p> Disconnect </p> 
+			</div>)
+			: (
+				<div className='wallet-container' onClick={() => handleMetamaskButtonClicked()} >
+					<img className="wallet-container__image" src="metamask.png" alt="Metamask Logo" />
+					 <p> Connect Wallet </p> 
+				</div>) 
+				}
 		</div>
 	)
 }
