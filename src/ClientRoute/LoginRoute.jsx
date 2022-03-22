7a90ed4725c563fab2a0c0292c950610bf4a248b
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-03-22 17:15:04 +1300

    Routing when logged in

---
+++ src/ClientRoute/LoginRoute.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { WalletContext } from '../Web3/context/WalletContextProvider';
+
+export default function LoginRoute(props) {
+
+	const { getConnectedStatus  } = useContext(WalletContext);
+
+	return getConnectedStatus() ? <Outlet /> : <Navigate to="/" />
+}
