fa2ba863f43d973b97049d162f7d2088bac0a435
unknown <kennyt12237@gmail.com>
2022-03-24 00:11:08 +1300

    Passed element as a prop into notification overlay

--- src/Notification/components/NotificationOverlay.jsx
+++ src/Notification/components/NotificationOverlay.jsx
@@ -2,11 +2,12 @@ import { useContext } from 'react';
 import  ReactDOM  from 'react-dom';
 import { NotificationContext } from '../context/NotificationContextProvider';
 
-export default function NotificationOverlay() {
+export default function NotificationOverlay(props) {
 
+    const { element } = props;
     const { getNotificationAlert } = useContext(NotificationContext);
 
     return (
-        ReactDOM.createPortal(getNotificationAlert(), document.getElementById('notification-root'))
+        ReactDOM.createPortal(getNotificationAlert(), element)
     )
 }
