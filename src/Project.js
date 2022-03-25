6ffb17b9791693abcbdfd59ab4ea95ede26c9e87
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-03-25 15:24:31 +1300

    Refactored notification module to include a custom hook

---
+++ src/Project.js
@@ -0,0 +1,41 @@
+const data =  {
+	projectContent : {
+		title : "Project One",
+		texts : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	},
+	web3Message : {
+		title : "Upvote and Write Message (optional)",
+		defaultText : "This is a message written",
+		imageUrl : "hand-thumbs-up-fill.svg",
+	},
+	mediaSrc : "cabin.png",
+}
+
+const projects =  [{
+	projectContent : {
+		title : "Project One",
+		texts : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	},
+	web3Message : {
+		title : "Upvote and Write Message (optional)",
+		defaultText : "This is a message written",
+		imageUrl : "hand-thumbs-up-fill.svg",
+	},
+	mediaSrc : "cabin.png",
+},
+{
+	projectContent : {
+		title : "Project Two",
+		texts : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+	},
+	web3Message : {
+		title : "Upvote and Write Message (optional)",
+		defaultText : "This is a message written",
+		imageUrl : "hand-thumbs-up-fill.svg",
+	},
+	mediaSrc : "leaf.png",
+}]
+
+export {
+	data, projects
+}
