c7b38de94796ecc8a854dc7371a08c182197231a
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-03-26 14:43:44 +1300

    Added css modifications for header when adjustment screen

--- src/Project.js
+++ src/Project.js
@@ -1,41 +1,36 @@
-const data =  {
-	projectContent : {
-		title : "Project One",
-		texts : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	},
-	web3Message : {
-		title : "Upvote and Write Message (optional)",
-		defaultText : "This is a message written",
-		imageUrl : "hand-thumbs-up-fill.svg",
-	},
-	mediaSrc : "cabin.png",
-}
+const projects = [
+  {
+    projectContent: {
+      title: "Project One",
+      texts: [
+        "Accomplished creating a modern web application for the product owner using the Scrum framework.",
+        "Satisfactory creation of application with new development tools with the team.",
+        "Successful adaptation to changing scenarios.",
+        "Extensive use of GitHub utilising the shared repository development model with 30+ branches and 300+ commits.",
+      ],
+    },
+    web3Message: {
+      title: "Upvote and Write Message (optional)",
+      defaultText: "This is a message written",
+      imageUrl: "hand-thumbs-up-fill.svg",
+    },
+    mediaSrc: "cabin.png",
+  },
+  {
+    projectContent: {
+      title: "Project Two",
+      texts: ["Accomplished creating an ambitious web application project with two team members.",
+      "Development using modern web development tools; the MERN stack and external APIs.",
+      "Successfully handled and extracted appropriate data from external API that linked with multiple components.",
+      "Proud accomplishment with a final presentation of the application to an audience."]
+    },
+    web3Message: {
+      title: "Upvote and Write Message (optional)",
+      defaultText: "This is a message written",
+      imageUrl: "hand-thumbs-up-fill.svg",
+    },
+    mediaSrc: "leaf.png",
+  },
+];
 
-const projects =  [{
-	projectContent : {
-		title : "Project One",
-		texts : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	},
-	web3Message : {
-		title : "Upvote and Write Message (optional)",
-		defaultText : "This is a message written",
-		imageUrl : "hand-thumbs-up-fill.svg",
-	},
-	mediaSrc : "cabin.png",
-},
-{
-	projectContent : {
-		title : "Project Two",
-		texts : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-	},
-	web3Message : {
-		title : "Upvote and Write Message (optional)",
-		defaultText : "This is a message written",
-		imageUrl : "hand-thumbs-up-fill.svg",
-	},
-	mediaSrc : "leaf.png",
-}]
-
-export {
-	data, projects
-}
+export { projects };
