c33487364f33b37491193e446ab8a5d13b8c1220
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-03-28 16:09:36 +1300

    Added smart contract functionality and API

--- src/Media/Components/Web3Message.jsx
+++ src/Media/Components/Web3Message.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "../scss/Web3Message.scss";
+import useSmartContract from "../../Web3/hooks/useSmartContract";
+import { websiteApprovalContract } from "../../smartContract";
 
 export default function Web3Message(props) {
   const { title, defaultText, imageUrl } = props;
-
+  const smartContract = useSmartContract(websiteApprovalContract);
+  
   return (
     <div className="web3-message-container">
       <div className="web3-message-container__title"> {title} </div>
@@ -13,15 +16,14 @@ export default function Web3Message(props) {
           type="text"
           placeholder={defaultText}
         />
-        <div className="web3-message-container__interactive__button">
+        <div className="web3-message-container__interactive__button" onClick={() => console.log(smartContract)}>
           <img
             className="web3-message-container__interactive__button__image"
             src={imageUrl}
             alt="Thumbs Up Button"
           />
           <p className="web3-message-container__interactive__button__text">
-            {" "}
-            Upvote{" "}
+            Upvote
           </p>
         </div>
       </div>
