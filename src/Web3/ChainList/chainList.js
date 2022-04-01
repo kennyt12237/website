8cf89940c413bd09336f9217115ed9f1493ebb6c
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-04-01 17:25:53 +1300

    Fixed an issue where chain id did not compare correctly

--- src/Web3/ChainList/chainList.js
+++ src/Web3/ChainList/chainList.js
@@ -3,10 +3,12 @@ const validChainList = {
 };
 
 const checkSupportedChain = (chainId) => {
-  const validChainKeys = Object.keys(validChainList);
-  for (let i = 0; i < validChainKeys.length; i++) {
-    if (validChainKeys[i] === chainId) {
-      return true;
+  if (chainId) {
+    const validChainKeys = Object.keys(validChainList);
+    for (let i = 0; i < validChainKeys.length; i++) {
+      if (validChainKeys[i] === chainId) {
+        return true;
+      }
     }
   }
 
