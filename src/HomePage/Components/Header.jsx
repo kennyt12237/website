a515732cab5cb564d1c81afa08760362377d6447
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-04-04 16:11:38 +1200

    Added logo and name to header

--- src/HomePage/Components/Header.jsx
+++ src/HomePage/Components/Header.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import MetamaskButton from "./MetamaskButton";
 import "../scss/Header.scss";
 export default function Header(props) {
+    const {logo, name} = props;
   return (
     <div className="header-container">
       <div className="logo-container">
         {/* Image logo here */}
-        <div className="logo-container__logo"> </div>
-        <div className="logo-container__name"> </div>
+        <img className="logo-container__logo" src={logo} alt="logo"/>
+        <div className="logo-container__name"> {name} </div>
       </div>
       <MetamaskButton />
     </div>
