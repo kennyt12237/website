af3eda0e154958de0b949cc931da9d2f04669cb6
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-04-08 18:10:37 +1200

    Added multiple project content

--- src/Media/Components/ProjectContent.jsx
+++ src/Media/Components/ProjectContent.jsx
@@ -1,28 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProjectText from "./ProjectText.jsx";
 import WriteAndUpvoteWeb3 from "./WriteAndUpvoteWeb3.jsx";
 import Media from "./Media.jsx";
 import "../scss/ProjectContent.scss";
 
 export default function ProjectContent(props) {
-  const { projectContent, mediaSrc, projectNumber } = props;
+  const { projectContent, projectNumber } = props;
+  const [contentNumber, setContentNumber] = useState(0);
 
+  const changeContentNumber = (number) => {
+    setContentNumber(number);
+  };
+
+  const getStyles = (index) => {
+    if (contentNumber === index) {
+      return {
+        backgroundColor: "white",
+      };
+    }
+  };
+
+  useEffect(() => {
+    setContentNumber(0);
+  }, [projectNumber]);
   return (
     <div className="project-content-container">
-      <div className="project-content-container__text">
-        <ProjectText
-          title={projectContent.title}
-          texts={projectContent.texts}
-        />
-        <WriteAndUpvoteWeb3
-          title="Upvote and Write Message (optional)"
-          defaultText="Wrtie a Message"
-          imageUrl="./hand-thumbs-up-fill.svg"
-          projectNumber={projectNumber}
-        />
+      <div className="project-content-container__content">
+        <div className="project-content-container__content__text">
+          <ProjectText
+            title={projectContent.title}
+            text={projectContent.contents[contentNumber].text}
+          />
+          <WriteAndUpvoteWeb3
+            title="Upvote and Write Message (optional)"
+            defaultText="Wrtie a Message"
+            imageUrl="./hand-thumbs-up-fill.svg"
+            projectNumber={projectNumber}
+          />
+        </div>
+        <div className="project-content-container__content__media">
+          <Media imageUrl={projectContent.contents[contentNumber].image} />
+        </div>
       </div>
-      <div className="project-content-container__media">
-        <Media imageUrl={mediaSrc} />
+      <div className="project-content-container__navigate">
+        {projectContent.contents.map((value, index) => {
+          return (
+            <div
+              className="project-content-container__navigate__item"
+              style={getStyles(index)}
+              onClick={() => changeContentNumber(index)}
+              key={index}
+            ></div>
+          );
+        })}
       </div>
     </div>
   );
