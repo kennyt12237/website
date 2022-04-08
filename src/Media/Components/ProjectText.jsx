d8ed89f4cfc1f0e88d4e226e898618a6a26e90a2
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-04-08 18:10:37 +1200

    Added multiple project content

--- src/Media/Components/ProjectText.jsx
+++ src/Media/Components/ProjectText.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import "../scss/ProjectText.scss";
 
 export default function ProjectText(props) {
-  const { title, texts } = props;
+  const { title, text } = props;
 
   return (
     <div className="project-text-container">
       <div className="project-text-container__title"> {title} </div>
-      <div className="project-text-container__text">
-      {texts && texts.map((text, index) => {
-          return <p className="project-text-container__text__point" key={index}> {text} </p>
-      }) }
-      </div>
+      <div className="project-text-container__text">{text}</div>
     </div>
   );
 }
