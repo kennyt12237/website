46208093c7046fc59b6d8b29caac18330487d2ba
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-04-08 18:10:37 +1200

    Added multiple project content

--- src/Contracts/Project.js
+++ src/Contracts/Project.js
@@ -1,40 +1,55 @@
 const projects = [
-  {
-    projectContent: {
+    {
       title: "TATWorks",
-      texts: [
-        "Accomplished creating a modern web application for the product owner using the Scrum framework.",
-        "Satisfactory creation of application with new development tools with the team.",
-        "Successful adaptation to changing scenarios.",
-        "Extensive use of GitHub utilising the shared repository development model with 30+ branches and 300+ commits.",
+      contents: [
+        {
+          text: "A redesigned web application for TatWorks utilising modern software tools",
+          image: "tatworks_landing_page.png",
+        },
+        {
+          text: "Full functionality implemented of the application requirements",
+          image: "tatworks_landing_page.png",
+        },
+        {
+          text: "Modularised code for reusability and maintainability of similar components",
+          image: "tatworks_landing_page.png",
+        },
       ],
     },
-    mediaSrc: "tatworks_landing_page.png",
-  },
-  {
-    projectContent: {
+    {
       title: "Are We There Yet",
-      texts: [
-        "Accomplished creating an ambitious web application project with two team members.",
-        "Development using modern web development tools; the MERN stack and external APIs.",
-        "Successfully handled and extracted appropriate data from external API that linked with multiple components.",
-        "Proud accomplishment with a final presentation of the application to an audience.",
+      contents: [
+        {
+          text: "An ambitious itinerary trip planner with visualisation at destinations",
+          image: "awty-map.png",
+        },
+        {
+          text: "Successfully handled and extracted data from external API that linked with multiple components.",
+          image: "awty-map.png",
+        },
+        {
+          text: "Development using modern web development tools; the MERN stack and external APIs.",
+          image: "awty-map.png",
+        },
       ],
     },
-    mediaSrc: "awty-map.png",
-  },
-  {
-    projectContent: {
+    {
       title: "Mello",
-      texts: [
-        "Accomplished creating a team proposed web application with 20 team members in two weeks.",
-        "Development using modern web development tools and frameworks.",
-        "Successful development and integration of a pop-up modal with team members code.",
-        "Extensive use of GitHub utilising the fork and pull request workflow model with over 100+ issues, 140+ pull requests, and a two person code review per pull request.",
+      contents: [
+        {
+          text: "A task management web application catering for students",
+          image: "mello_homepage.png",
+        },
+        {
+          text: "Integration of Spotify API with a Pomodoro Technique Timer mechanism",
+          image: "mello_homepage.png",
+        },
+        {
+          text: "Sofware development using modern web development tools and frameworks.",
+          image: "mello_homepage.png",
+        },
       ],
     },
-    mediaSrc: "mello_homepage.png",
-  },
-];
-
-export { projects };
+  ];
+  
+  export { projects };
