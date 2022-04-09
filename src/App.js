7ead2d0fa594ea41b047b1e93bacf9b1b264bd92
unknown <kennyt12237@gmail.com>
2022-04-09 17:53:17 +1200

    Modified routes

--- src/App.js
+++ src/App.js
@@ -5,34 +5,34 @@ import {
   Routes,
   Route,
   Outlet,
+  Navigate,
 } from "react-router-dom";
 import HomeContent from "./HomePage/Components/HomeContent";
 import ProjectContentList from "./Media/Components/ProjectContentList";
 import Header from "./HomePage/Components/Header";
 import { projects } from "./Contracts/Project";
 import LoginRoute from "./ClientRoute/LoginRoute";
-import ErrorPage from "./ErrorPage";
 import { NotificationOverlay } from "./Notification";
 import ChainRoute from "./ClientRoute/ChainRoute";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/website" element={<Layout />}>
           <Route index element={<HomeContent />} />
           <Route element={<LoginRoute />}>
             <Route element={<ChainRoute />}>
               <Route
-                path="/projects"
+                path="projects"
                 element={<ProjectContentList projects={projects} />}
               />
             </Route>
           </Route>
-          <Route
-            path="/error"
-            element={<ErrorPage errMsg={"Connect to metamask"} />}
-          />
         </Route>
+        <Route
+            path="*"
+            element={<Navigate to="/website"/>}
+          />
       </Routes>
     </Router>
   );
