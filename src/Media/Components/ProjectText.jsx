32f5cc87d59bdd22ee8ff2f6b9f38ba4a890dd76
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-04-09 12:32:50 +1200

    Revert "Added multiple project content"
    This reverts commit b2c6b1f2406eb56d216a29cc13e8b16e0fac1b86.
    
    everting Commit

--- src/Media/Components/ProjectText.jsx
+++ src/Media/Components/ProjectText.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "../scss/ProjectText.scss";
 
 export default function ProjectText(props) {
-  const { title, text } = props;
+  const { title, texts } = props;
 
   return (
     <div className="project-text-container">
       <div className="project-text-container__title"> {title} </div>
-      <div className="project-text-container__text">{text}</div>
+      <div className="project-text-container__text">
+      {texts && texts.map((text, index) => {
+          return <p className="project-text-container__text__point" key={index}> {text} </p>
+      }) }
+      </div>
     </div>
   );
 }
