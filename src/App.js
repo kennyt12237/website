3ae014acc089395c5999199e94ae38853e98d24e
unknown <kennyt12237@gmail.com>
2022-06-20 21:10:01 +1200

    Cleaned App.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import { AppRoute } from "./ClientRoute";
 import { projects } from "./Contracts/Project";
-import store from './Store';
 import "./App.scss";
 
 function App() {
-  console.log('Initial State ', store.getState())
-  return (
-    <AppRoute projects={projects} />
-  );
+  return <AppRoute projects={projects} />;
 }
 
 export default App;
