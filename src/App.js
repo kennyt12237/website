c0b2e2a5ce590e3fe49bfc01a8ba273e79acaeef
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-06-20 15:22:24 +1200

    Refactored routes to AppRoute component and in ClientRoute module

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import React from "react";
-import "./App.scss";
-import {
-  HashRouter as Router,
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
-import HomeContent from "./HomePage/Components/HomeContent";
-import ProjectContentList from "./Media/Components/ProjectContentList";
-import { Layout } from './HomePage';
+import { AppRoute } from "./ClientRoute";
 import { projects } from "./Contracts/Project";
-import LoginRoute from "./ClientRoute/LoginRoute";
-import { NotificationOverlay } from "./Notification";
-import ChainRoute from "./ClientRoute/ChainRoute";
+import "./App.scss";
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/website" element={<Layout NotificationOverlay={<NotificationOverlay id={"notification-root"}/>} />}>
-          <Route index element={<HomeContent />} />
-          <Route element={<LoginRoute />}>
-            <Route element={<ChainRoute />}>
-              <Route
-                path="projects"
-                element={<ProjectContentList projects={projects} />}
-              />
-            </Route>
-          </Route>
-        </Route>
-        <Route
-            path="*"
-            element={<Navigate to="/website" />}
-          />
-      </Routes>
-    </Router>
+    <AppRoute projects={projects} />
   );
 }
 
