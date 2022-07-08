07046f7bcb8b191ec43defb96334ae8e06d2e2d8
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-07-08 18:12:49 +1200

    Added trial ethers method

--- src/HomePage/Components/MetamaskButton.jsx
+++ src/HomePage/Components/MetamaskButton.jsx
@@ -1,17 +1,30 @@
 import React, { useEffect } from "react";
 import { WebsiteWithMetamaskProvider } from "../../Website";
-import { selectWalletProvider } from '../../Redux';
+import WebsiteAPI from "../../Contracts/WebsiteAPI";
+import { websiteContract } from "../../Contracts/websiteContract";
+import { selectWalletProvider } from "../../Redux";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { ethers } from "ethers";
 
 export default function MetamaskButton() {
-  const { handleConnectButtonClicked, handleDisconnectButtonClicked } =
-    WebsiteWithMetamaskProvider();
+  const {
+    provider,
+    handleConnectButtonClicked,
+    handleDisconnectButtonClicked,
+  } = WebsiteWithMetamaskProvider();
   const navigate = useNavigate();
   const walletProvider = useSelector(selectWalletProvider);
 
   useEffect(() => {
     if (walletProvider.isConnected) {
+      const getContract = async () => {
+        const ethersProvider = new ethers.providers.Web3Provider(provider)
+        const res = new ethers.Contract(websiteContract.address, websiteContract.abi, ethersProvider);
+        return res;
+      };
+      const contract = getContract();
+      contract.then((res) => console.log(res));
       navigate("/website/projects");
     }
   }, [walletProvider]);
