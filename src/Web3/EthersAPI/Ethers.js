a23bf73cd8c50ca76e64b9394952436ffb1b9906
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-07-18 15:54:11 +1200

    Added EthersJs custom API for easy use with methods

---
+++ src/Web3/EthersAPI/Ethers.js
@@ -0,0 +1,17 @@
+import ethers from 'ethers';
+
+export default function Ethers() {
+
+    const getWrappedProvider = (provider) => {
+        return new ethers.providers.Web3Provider(provider);
+    }
+
+    const getContract = (contractAddress, contractAbi, wrappedProvider) => {
+        return new ethers.Contract(contractAddress, contractAbi, wrappedProvider);
+    }
+
+    return {
+        getWrappedProvider,
+        getContract
+    }
+}
