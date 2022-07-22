1e9c296b24a97d3d29ef87f0fd1173cde7151e20
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-07-22 17:07:10 +1200

    Updates

---
+++ precache-manifest.14fb9fc4c7de49f690daf52f907099fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca5a940ad9a7e02e4c6f",
+    "url": "https://kennyt12237.github.io/website/static/css/main.f14986bf.chunk.css"
+  },
+  {
+    "revision": "ca5a940ad9a7e02e4c6f",
+    "url": "https://kennyt12237.github.io/website/static/js/main.ca5a940a.chunk.js"
+  },
+  {
+    "revision": "aa1ecd07d8b85afa671e",
+    "url": "https://kennyt12237.github.io/website/static/js/1.aa1ecd07.chunk.js"
+  },
+  {
+    "revision": "486b168d0340ff09bdf4",
+    "url": "https://kennyt12237.github.io/website/static/js/2.486b168d.chunk.js"
+  },
+  {
+    "revision": "8d7d5788693ef9b6e542",
+    "url": "https://kennyt12237.github.io/website/static/js/runtime~main.8d7d5788.js"
+  },
+  {
+    "revision": "2e6a764adb64f13b3125c94372c45637",
+    "url": "https://kennyt12237.github.io/website/static/media/coinbase.2e6a764a.svg"
+  },
+  {
+    "revision": "852415449af17f3b18a6a6f4e1f20627",
+    "url": "https://kennyt12237.github.io/website/index.html"
+  }
+];
