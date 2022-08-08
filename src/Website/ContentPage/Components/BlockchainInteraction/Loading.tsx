efcfe7cfbbe35022ddafd6b803ef87a04bf8bbbc
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-08-08 14:56:42 +1200

    Added types for src/Website/ContentPage/BlockchainInteraction for some files

--- src/Website/ContentPage/Components/BlockchainInteraction/Loading.tsx
+++ src/Website/ContentPage/Components/BlockchainInteraction/Loading.tsx
@@ -1,11 +1,13 @@
 import React, { useMemo } from "react";
 import "./scss/Loading.scss";
 
+interface Props {
+  children: JSX.Element;
+  loading: boolean;
+}
 // A higher order component to display a loading indication for a state variable.
-export default function Loading(props) {
-  const { children, loading } = props;
-
-  const isLoading = useMemo(() => {
+export default function Loading({ children, loading }: Props): JSX.Element {
+  const isLoading: boolean = useMemo(() => {
     return loading;
   }, [loading]);
 
