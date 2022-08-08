6bce2b6e6545a4290bab8c2f9d7b56cb39d4809d
kennyt12237 <42660433+kennyt12237@users.noreply.github.com>
2022-08-08 17:58:05 +1200

    Typescript implementation (#18)
    * Added typescript dependencies, and modified a file to conform with typescript
    
    * Converted to typescript for remaining LandingPage module
    
    * Changed all index files to typescript (excluding the root index)
    
    * Converted Website/ContentPage/Contracts module to typescript
    
    * Changed Notification module to typescript
    
    * Added types to majority of the Web3 modules
    
    * Added types for src/Website/ClientRoute module
    
    * Added types to the remaining Web3 module
    
    * Added types for src/Website/ContentPage/Components components for partial files
    
    * Added types for src/Website/ContentPage/BlockchainInteraction for some files
    
    * Added types for remaining src/Website/ContentPage/BlockchainInteraction files, and modified types from other files for compatibility
    
    * Converted remaining src/ files to typescript
    
    * Fixed issues with other files when converting remaining src/ files

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { AppRoute, projects } from "./Website";
 import "./App.scss";
 
-function App() {
+function App(): JSX.Element {
   return <AppRoute projects={projects} />;
 }
 
